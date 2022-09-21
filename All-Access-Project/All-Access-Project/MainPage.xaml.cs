f5209001107e19d25e34f2a3e8e847c8b636a0bc
Mikala <mikalasimons@gmail.com>
2022-09-20 20:39:45 -0400

    Changing to blank
    Changing to a blank format because the flyout version was too complicated

---
+++ All-Access-Project/All-Access-Project/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace All_Access_Project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
