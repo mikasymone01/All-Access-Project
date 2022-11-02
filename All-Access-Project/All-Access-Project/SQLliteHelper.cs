640a801f33c1afff8aacef435109421acc441b64
dbaker1990 <dominicbaker16@gmail.com>
2022-11-02 16:04:01 -0400

    ok to pull but right now it is incomplete
    incomplete for now

---
+++ All-Access-Project/All-Access-Project/SQLliteHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+
+
+namespace All_Access_Project
+{
+    internal class SQLliteHelper
+    {
+        private readonly SQLiteAsyncConnection db;
+        public SQLliteHelper(string dbPath)
+        {
+            db = new SQLiteAsyncConnection(dbPath);
+            db.CreateTableAsync<UserAccounts>();
+        }
+
+        public Task<int> CreateAccount(UserAccounts account)
+        {
+            return db.InsertAsync(account);
+        }
+
+        public Task<List<UserAccounts>>ReadAccounts()
+        {
+            return db.Table<UserAccounts>().ToListAsync();
+        }
+
+        public Task<int> UpdateAccount(UserAccounts accounts)
+        {
+            return db.UpdateAsync(accounts);
+        }
+
+        public Task<int> DeleteAccount(UserAccounts account)
+        {
+            return db.DeleteAsync(account);
+        }
+    }
+    
+}
---
+++ All-Access-Project/All-Access-Project/UserAccounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace All_Access_Project
+{
+    internal class UserAccounts
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}
